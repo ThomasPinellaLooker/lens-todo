5e64a1ddb0018f7ae45ea6f807c928fe66aa9eba
thomas.pinella@looker.com <thomas.pinella@looker.com>
2018-12-04 10:25:09 -0800

    Added a couple tests

--- src/components/AddTodo/AddTodo.test.tsx
+++ src/components/AddTodo/AddTodo.test.tsx
@@ -1,14 +1,31 @@
+import { mount } from 'enzyme';
 import { theme, ThemeProvider } from 'looker-lens';
 import * as React from 'react'
 import { create } from 'react-test-renderer';
+import "../../../test/setupJest.js"
 import { AddTodo } from './AddTodo';
 
 test('Default AddTodo', () => {
+  const addTodo = () => undefined
+
   const component = create(
     <ThemeProvider theme={theme}>
-      <AddTodo addTodo={() => undefined} />
+      <AddTodo addTodo={addTodo} />
     </ThemeProvider>
   )
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
+})
+
+test('Add button is disabled initially', () => {
+  const addTodo = () => undefined
+
+  const wrapper = mount(
+    <ThemeProvider theme={theme}>
+      <AddTodo addTodo={addTodo} />
+    </ThemeProvider>
+  )
+
+  const addButton = wrapper.find('button')
+  expect(addButton.props().disabled).toBe(true)
 })
